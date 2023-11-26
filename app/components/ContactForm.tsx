7ef6a7fe1cbdc03cf7d3adda7c4994bc31e002fe
adir-barak <adir.barak@mail.huji.ac.il>
2023-11-26 14:43:27 +0200

    commented out some of the unused code

--- app/components/ContactForm.tsx
+++ app/components/ContactForm.tsx
@@ -1,83 +1,44 @@
-'use client';
-import { useState } from 'react';
-import { PaperPlaneTilt } from '@phosphor-icons/react/dist/ssr';
-import { handleContactFormSubmission } from '../lib/actions';
-import ThankYou from './ThankYou';
+// 'use client';
+// import { useState } from 'react';
+// import { PaperPlaneTilt } from '@phosphor-icons/react/dist/ssr';
+// import { handleContactFormSubmission } from '../lib/actions';
+// import ThankYou from './ThankYou';
 type Props = {};
 
 export default function ContactForm({}: Props) {
-  const [formSubmitted, setFormSubmitted] = useState(false);
-  return (
-    <>
-      <ThankYou />
-      <form
-        className={`contact-form ${formSubmitted && 'hidden'}`}
-        action={handleContactFormSubmission}
-        onSubmit={(e) => {
-          setFormSubmitted(true);
-        }}>
-        <div className='contact-form-info'>
-          <div>
-            <label htmlFor='name' className='form-label'>
-              Name:
-            </label>
-            <input id='name' name='name' type='text' className='form-input' required />
-            <label htmlFor='email' className='form-label'>
-              Email:
-            </label>
-            <input id='email' name='email' type='email' className='form-input' required />
-          </div>
-          <div className='contact-form-message'>
-            <label htmlFor='message' className='form-label'>
-              Message:
-            </label>
-            <textarea id='message' name='message' className='form-input textarea' />
-          </div>
-        </div>
-        <button className='contact-form-button button'>
-          Send
-          <PaperPlaneTilt className='button-icon' />
-        </button>
-      </form>
-    </>
-    // <>
-    //   {formSubmitted ? (
-    //     <div className='thank-you'>
-    //       Thanks!
-    //       <br />
-    //       I'll get back to you ASAP.
-    //     </div>
-    //   ) : (
-    //     <form
-    //       className='contact-form'
-    //       action={handleContactFormSubmission}
-    //       onSubmit={(e) => {
-    //         setFormSubmitted(true);
-    //       }}>
-    //       <div className='contact-form-info'>
-    //         <div>
-    //           <label htmlFor='name' className='form-label'>
-    //             Name:
-    //           </label>
-    //           <input id='name' name='name' type='text' className='form-input' required />
-    //           <label htmlFor='email' className='form-label'>
-    //             Email:
-    //           </label>
-    //           <input id='email' name='email' type='email' className='form-input' required />
-    //         </div>
-    //         <div className='contact-form-message'>
-    //           <label htmlFor='message' className='form-label'>
-    //             Message:
-    //           </label>
-    //           <textarea id='message' name='message' className='form-input textarea' />
-    //         </div>
-    //       </div>
-    //       <button className='contact-form-button button'>
-    //         Send
-    //         <PaperPlaneTilt className='button-icon' />
-    //       </button>
-    //     </form>
-    //   )}
-    // </>
-  );
+  // const [formSubmitted, setFormSubmitted] = useState(false);
+  return <></>;
 }
+// return (
+//   <>
+//     <ThankYou />
+//     <form
+//       className={`contact-form ${formSubmitted && 'hidden'}`}
+//       action={handleContactFormSubmission}
+//       onSubmit={(e) => {
+//         setFormSubmitted(true);
+//       }}>
+//       <div className='contact-form-info'>
+//         <div>
+//           <label htmlFor='name' className='form-label'>
+//             Name:
+//           </label>
+//           <input id='name' name='name' type='text' className='form-input' required />
+//           <label htmlFor='email' className='form-label'>
+//             Email:
+//           </label>
+//           <input id='email' name='email' type='email' className='form-input' required />
+//         </div>
+//         <div className='contact-form-message'>
+//           <label htmlFor='message' className='form-label'>
+//             Message:
+//           </label>
+//           <textarea id='message' name='message' className='form-input textarea' />
+//         </div>
+//       </div>
+//       <button className='contact-form-button button'>
+//         Send
+//         <PaperPlaneTilt className='button-icon' />
+//       </button>
+//     </form>
+//   </>
